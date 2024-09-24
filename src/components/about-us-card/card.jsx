8304c8cb77b75a87ac974608baab77bc3762f085
felipe santos <felipesantosmarcelino2004@gmail.com>
2024-09-24 17:10:39 -0300

    improviment: add img alt text

--- src/components/about-us-card/card.jsx
+++ src/components/about-us-card/card.jsx
@@ -1,20 +1,19 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-export const Card = ({ title, description, image, imageAlt }) => {
+export const Card = ({ title, description, image }) => {
   return (
-    <div className='card'>
-      <h3 className='card__title'>{title}</h3>
-      <p className='card__description'>{description}</p>
-      <div className='card__wrapper-img'>
-        <img src={image} alt={imageAlt} />
+    <div className="card">
+      <h3 className="card__title">{title}</h3>
+      <p className="card__description">{description}</p>
+      <div className="card__wrapper-img">
+        <img src={image} alt="icone" />
       </div>
     </div>
-  )
-}
+  );
+};
 
 Card.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
-  imageAlt: PropTypes.string.isRequired
 };
