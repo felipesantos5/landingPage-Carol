4fc4e26cd72cfceb9f16ecd122e3b9175b04d53d
felipe santos <felipesantosmarcelino2004@gmail.com>
2024-09-23 22:02:11 -0300

    feat: remove a text

--- src/components/sections/about-us.jsx
+++ src/components/sections/about-us.jsx
@@ -8,12 +8,14 @@ export const AboutUs = () => {
         <div className="about-us__text" id="about-us">
           <h2 className="about-us__text-title">Sobre nós</h2>
           <p className="about-us__text-paragraph1">
-            Impulsione o sucesso da sua empresa com nossos serviços especializados. Transforme desafios em oportunidades e faça seu negócio alcançar novos patamares com a nossa expertise!
+            Impulsione o sucesso da sua empresa com nossos serviços especializados. Transforme desafios em oportunidades e faça seu negócio alcançar novos patamares com a nossa expertise. Com anos de
+            experiência no mercado digital, nossa equipe oferece soluções personalizadas para alavancar a presença online do seu negócio. Desde a criação de conteúdo envolvente para redes sociais até
+            a implementação de estratégias de tráfego pago que realmente convertem, estamos aqui para ajudá-lo a atingir seus objetivos.
           </p>
-          <p className="about-us__text-paragraph2">
+          {/* <p className="about-us__text-paragraph2">
             Com anos de experiência no mercado digital, nossa equipe oferece soluções personalizadas para alavancar a presença online do seu negócio. Desde a criação de conteúdo envolvente para redes
             sociais até a implementação de estratégias de tráfego pago que realmente convertem, estamos aqui para ajudá-lo a atingir seus objetivos.
-          </p>
+          </p> */}
           <div className="about-us__services" id="services">
             <h2 className="about-us__services-title">Serviços Oferecidos</h2>
             <p className="about-us__services-description">
