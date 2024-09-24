ab130d27921f451a750da30698c44a33ed45e30e
felipe santos <felipesantosmarcelino2004@gmail.com>
2024-09-23 22:02:04 -0300

    fix: make client changes

--- src/components/sections/footer.jsx
+++ src/components/sections/footer.jsx
@@ -7,38 +7,40 @@ import linkedin from "../../assets/header/linkedin.svg";
 
 export const Footer = () => {
   return (
-    <footer className="footer">
+    <div className="footer-padding">
       <Slider />
-      <footer className="footer__wrapper">
-        <img src={logo} alt="logo Carol Quintano" className="footer__logo" />
+      <footer className="footer">
+        <footer className="footer__wrapper">
+          <img src={logo} alt="logo Carol Quintano" className="footer__logo" />
 
-        <p className="footer__number">(48) 99944-3959</p>
-        <p className="footer__email">agenciacarolquintana@gmail.com</p>
-        <p className="footer__cep">Florianópolis - Santa Catarina</p>
+          <p className="footer__number">(48) 99944-3959</p>
+          <p className="footer__email">agenciacarolquintana@gmail.com</p>
+          <p className="footer__cep">Florianópolis - Santa Catarina</p>
 
-        <ul className="footer__option-wrapper">
-          <li className="footer__item">Social Media</li>
-          <li className="footer__item">Landing Page</li>
-          <li className="footer__item">WebSite</li>
-          <li className="footer__item">Tráfego Pago</li>
-          <li className="footer__item">Assessoria Comercial</li>
-          <li className="footer__item">Prospecção Fria</li>
-        </ul>
+          <ul className="footer__option-wrapper">
+            <li className="footer__item">Social Media</li>
+            <li className="footer__item">Landing Page</li>
+            <li className="footer__item">WebSite</li>
+            <li className="footer__item">Tráfego Pago</li>
+            <li className="footer__item">Assessoria Comercial</li>
+            <li className="footer__item">Prospecção Fria</li>
+          </ul>
 
-        <div className="footer__midia-wrapper">
-          <a href="">
-            <img src={instagram} alt="instagram" />
-          </a>
-          <a href="">
-            <img src={whatsapp} alt="whatsapp" />
-          </a>
-          <a href="">
-            <img src={linkedin} alt="linkedin" />
-          </a>
-        </div>
+          <div className="footer__midia-wrapper">
+            <a href="">
+              <img src={instagram} alt="instagram" />
+            </a>
+            <a href="">
+              <img src={whatsapp} alt="whatsapp" />
+            </a>
+            <a href="">
+              <img src={linkedin} alt="linkedin" />
+            </a>
+          </div>
 
-        <p className="footer__credits">Carol Quintana - 2024 - Todos os direitos reservados.</p>
+          <p className="footer__credits">Carol Quintana - 2024 - Todos os direitos reservados.</p>
+        </footer>
       </footer>
-    </footer>
+    </div>
   );
 };
