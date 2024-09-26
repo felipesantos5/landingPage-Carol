2752e8a636153883a3628fba5f8d2ca19ac1f5a2
felipe santos <felipesantosmarcelino2004@gmail.com>
2024-09-26 00:11:37 -0300

    test: put a iframed loaded in iframe youtube

--- src/components/sections/about-us.jsx
+++ src/components/sections/about-us.jsx
@@ -1,6 +1,9 @@
 import shadow from "../../assets/shadow-about-us.svg";
+import { useState } from "react";
 
 export const AboutUs = () => {
+  const [isIframeLoaded, setIframeLoaded] = useState(false);
+
   return (
     <section className="about-us" id="#about-us">
       <img src={shadow} alt="" className="about-us__shadow" />
@@ -12,10 +15,6 @@ export const AboutUs = () => {
             experiência no mercado digital, nossa equipe oferece soluções personalizadas para alavancar a presença online do seu negócio. Desde a criação de conteúdo envolvente para redes sociais até
             a implementação de estratégias de tráfego pago que realmente convertem, estamos aqui para ajudá-lo a atingir seus objetivos.
           </p>
-          {/* <p className="about-us__text-paragraph2">
-            Com anos de experiência no mercado digital, nossa equipe oferece soluções personalizadas para alavancar a presença online do seu negócio. Desde a criação de conteúdo envolvente para redes
-            sociais até a implementação de estratégias de tráfego pago que realmente convertem, estamos aqui para ajudá-lo a atingir seus objetivos.
-          </p> */}
           <div className="about-us__services" id="services">
             <h2 className="about-us__services-title">Serviços Oferecidos</h2>
             <p className="about-us__services-description">
@@ -24,13 +23,16 @@ export const AboutUs = () => {
           </div>
         </div>
         <div className="video-container">
-          <iframe
-            src="https://www.youtube.com/embed/QAUzWtLMnU0"
-            title="Video youtube"
-            loading="lazy"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
+          {!isIframeLoaded && <img src="https://img.youtube.com/vi/QAUzWtLMnU0/maxresdefault.jpg" alt="YouTube Thumbnail" className="video-thumbnail" onClick={() => setIframeLoaded(true)} />}
+          {isIframeLoaded && (
+            <iframe
+              src="https://www.youtube.com/embed/QAUzWtLMnU0?autoplay=1"
+              title="Video youtube"
+              loading="lazy"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          )}
         </div>
       </div>
     </section>
