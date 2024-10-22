8608a7804a65931f80d0c7dab3424b2447022693
felipe santos <felipesantosmarcelino2004@gmail.com>
2024-10-21 23:51:01 -0300

    feat: create animation section component

---
+++ src/components/animated-section/animatedSection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const AnimatedSection = ({ children, className }) => {
+  return (
+    <motion.section initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.6, ease: "easeOut" }} viewport={{ once: true, amount: 0.2 }} className={className}>
+      {children}
+    </motion.section>
+  );
+};
+
+export default AnimatedSection;
