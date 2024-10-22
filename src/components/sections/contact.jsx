e04c429d591332bde6f5d5ef7b7230d99a4afc37
felipe santos <felipesantosmarcelino2004@gmail.com>
2024-10-21 23:58:32 -0300

    feat: add animated section in contact

--- src/components/sections/contact.jsx
+++ src/components/sections/contact.jsx
@@ -1,10 +1,11 @@
 import { Form } from "../form-initial/form";
+import AnimatedSection from "../animated-section/animatedSection";
 
 export const Contact = () => {
   return (
-    <section className="contact">
+    <AnimatedSection className="contact">
       <h2 className="contact__title">Entre em contato</h2>
       <Form />
-    </section>
+    </AnimatedSection>
   );
 };
