74c021d127504f4f9573abe2f06e41d175ed558c
felipe santos <felipesantosmarcelino2004@gmail.com>
2024-10-24 00:00:58 -0300

    feat: add new section about us

--- src/components/sections/about-us.jsx
+++ src/components/sections/about-us.jsx
@@ -10,12 +10,47 @@ export const AboutUs = () => {
       <img src={shadow} alt="" className="about-us__shadow" />
       <div className="about-us__container">
         <div className="about-us__text" id="about-us">
-          <h2 className="about-us__text-title">Sobre nós</h2>
+          {/* <h2 className="about-us__text-title">Sobre nós</h2>
           <p className="about-us__text-paragraph1">
             Impulsione o sucesso da sua empresa com nossos serviços especializados. Transforme desafios em oportunidades e faça seu negócio alcançar novos patamares com a nossa expertise. Com anos de
             experiência no mercado digital, nossa equipe oferece soluções personalizadas para alavancar a presença online do seu negócio. Desde a criação de conteúdo envolvente para redes sociais até
             a implementação de estratégias de tráfego pago que realmente convertem, estamos aqui para ajudá-lo a atingir seus objetivos.
-          </p>
+          </p> */}
+          <div className="about-us__data-wrapper">
+            <h2 className="about-us-title-data">Apoiamos negócios brilhantes</h2>
+            <h3 className="about-us-subtitle-data">na transformação de mercados</h3>
+
+            <section className="about-us__data">
+              <div>
+                <div className="about-us__data-container">
+                  <img src="https://cdn.prod.website-files.com/61a3ca260fa4a5c7f6d7bf30/64e3d63af3da8bedaf0aa32f_clientes-atendidos.svg" alt="" />
+                  <p className="about-us__data-container-title">+100</p>
+                </div>
+                <p className="about-us__data-description">clientes atentidos</p>
+              </div>
+
+              <div className="about-us__data-line"></div>
+
+              <div>
+                <div className="about-us__data-container">
+                  <img src="https://cdn.prod.website-files.com/61a3ca260fa4a5c7f6d7bf30/64e3d63a8e1525ab709443a1_milhoes-investidos.svg" alt="" />
+                  <p className="about-us__data-container-title">+120</p>
+                </div>
+                <p className="about-us__data-description">milhões investidos</p>
+              </div>
+
+              <div className="about-us__data-line"></div>
+
+              <div>
+                <div className="about-us__data-container">
+                  <img src="https://cdn.prod.website-files.com/61a3ca260fa4a5c7f6d7bf30/64e3d63a20a5c9940b7cef27_geracao-receita.svg" alt="" />
+                  <p className="about-us__data-container-title">+1bi</p>
+                </div>
+                <p className="about-us__data-description">gerado de receita</p>
+              </div>
+            </section>
+          </div>
+
           <div className="about-us__services" id="services">
             <h2 className="about-us__services-title">Serviços Oferecidos</h2>
             <p className="about-us__services-description">
