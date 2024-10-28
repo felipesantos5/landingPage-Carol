ab53d8d976e9384272d0b3053b4e033c2034709d
felipe santos <felipesantosmarcelino2004@gmail.com>
2024-10-27 21:56:59 -0300

    feat: change transition time animation section

--- src/components/animated-section/animatedSection.jsx
+++ src/components/animated-section/animatedSection.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 
 const AnimatedSection = ({ children, className }) => {
   return (
-    <motion.section initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.6, ease: "easeOut" }} viewport={{ once: true, amount: 0.2 }} className={className}>
+    <motion.section initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 1, ease: "easeOut" }} viewport={{ once: true, amount: 0.4 }} className={className}>
       {children}
     </motion.section>
   );
